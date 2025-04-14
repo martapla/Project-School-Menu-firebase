9dbf11aadb8ba045dd123f41080be385410a79ce
martapla <martaplanella@hotmail.com>
2025-04-14 17:57:26 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,29 @@ import WeeklyPlan from './components/WeeklyPlan'
 
 
 function App() {
-  const {loading, error, filterMeals, handleFilter, handleSearch, handleSort, addNewMeal,setNewMealName,setNewMealDescription,setNewMealCategory,newMealName,newMealDescription,newMealCategory,handleEdit,handleDelete,weeklyPlan,addMealToWeeklyPlan,removeWeeklyPlan} = useFireBaseFetch()
-
+  const {loading, error, filterMeals, handleFilter, handleSearch, 
+    handleSort, addNewMeal,setNewMealName,setNewMealDescription,
+    setNewMealCategory,newMealName,newMealDescription,newMealCategory,
+    handleEdit,handleDelete,weeklyPlan,addMealToWeeklyPlan,
+    removeWeeklyPlan} = useFireBaseFetch()
 
   if (loading) return <h3 className='loading'>Loading...</h3>;
   if (error) return <h3>Error: {error}</h3>;
 
-
   return (
     <>
       <h1 className='title'>School Menu  🍽️ </h1>
-      <FilterAndSort handleFilter={handleFilter} handleSearch={handleSearch} handleSort={handleSort}/>
+      <FilterAndSort 
+        handleFilter={handleFilter} 
+        handleSearch={handleSearch} 
+        handleSort={handleSort}
+      />
       <MealsList 
         food={filterMeals} 
         handleEdit={handleEdit} 
         handleDelete={handleDelete} 
-        addMealToWeeklyPlan={addMealToWeeklyPlan}/>
+        addMealToWeeklyPlan={addMealToWeeklyPlan}
+      />
       <MealsEditor 
         addNewMeal={addNewMeal} 
         newMealName={newMealName} 
@@ -32,7 +39,10 @@ function App() {
         setNewMealDescription={setNewMealDescription}
         setNewMealCategory={setNewMealCategory}
       />
-      <WeeklyPlan weeklyPlan={weeklyPlan} removeWeeklyPlan={removeWeeklyPlan}/>
+      <WeeklyPlan 
+        weeklyPlan={weeklyPlan} 
+        removeWeeklyPlan={removeWeeklyPlan}
+      />
     </>
   )
 }
--- src/firebase/firebaseServer.js
+++ src/firebase/firebaseServer.js
@@ -5,13 +5,15 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDp55nOUyLh1ODxhMoZCvPexOh0GUcxrT0",
-  authDomain: "menu-escolar-1ee62.firebaseapp.com",
-  projectId: "menu-escolar-1ee62",
-  storageBucket: "menu-escolar-1ee62.firebasestorage.app",
-  messagingSenderId: "253359266064",
-  appId: "1:253359266064:web:9a8a553b6db9542bbded95"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
